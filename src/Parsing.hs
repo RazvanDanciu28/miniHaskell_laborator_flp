cfa4b8e8583203d84c2a280884995f4562f5caf4
RazvanDanciu28 <mariorazvan.2002@gmail.com>
2023-03-13 15:50:08 +0200

    done

--- src/Parsing.hs
+++ src/Parsing.hs
@@ -59,7 +59,7 @@ natExp = undefined
 -- Nat 223
 
 parenExp :: Parser ComplexExp
-parenExp = undefined
+parenExp = (parens miniHs) expr
 -- >>> ghci> testParse parenExp "(a)"
 -- CX (Var {getVar = "a"})
 
@@ -68,8 +68,14 @@ basicExp = undefined
 -- >>> testParse basicExp "[a,b,c]"
 -- List [CX (Var {getVar = "a"}),CX (Var {getVar = "b"}),CX (Var {getVar = "c"})]
 
+aux :: [ComplexExp] -> ComplexExp
+aux [x] = x
+aux (x:xs) = CApp x  $ aux xs
+
 expr :: Parser ComplexExp
-expr = varExp
+expr = do
+    es <- some (basicExp)
+    return $ foldl1 CApp es
 -- >>> testParse expr "\\x -> [x,y,z]"
 -- CLam (Var {getVar = "x"}) (List [CX (Var {getVar = "x"}),CX (Var {getVar = "y"}),CX (Var {getVar = "z"})])
 
